e4276deb20dffeb3e1dd1c61940cd2debbf8f003
Abigail <ard99@yahoo.com>
2023-09-26 09:53:55 -0400

    V3 Completed

--- routes/runs.js
+++ routes/runs.js
@@ -5,7 +5,19 @@ const Runs = require('models/Run');
 // GET runs page
 router.get('/', async (req, res) => {
     const runs = await Runs.findAll();
-    res.render('Runs', {title: "Runs", runsList: runs});
+    res.render('runs/index', {title: "Runs", runsList: runs});
+});
+
+// GET a single run
+router.get('/:id', async (req, res) => {
+    const requestedId = req.params.id;
+    const chosenRun = await Runs.findOne({
+        where: {id: requestedId}
+    });
+    res.render('runs/run', {
+        title: chosenRun.name + " Details",
+        runItem: chosenRun
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,19 @@ const Users = require('models/User');
 /* GET users listing. */
 router.get('/', async(req, res) => {
   const users = await Users.findAll();
-  res.render('Users', {title: "Users", usersList: users});
+  res.render('users/index', {title: "Users", usersList: users});
+});
+
+// GET a single user
+router.get('/:id', async (req, res) => {
+  const requestedId = req.params.id;
+  const chosenUser = await Users.findOne({
+      where: {id: requestedId}
+  });
+  res.render('users/user', {
+      title: chosenUser.first_name + " " + chosenUser.last_name,
+      userItem: chosenUser
+  });
 });
 
 module.exports = router;
